03a234ea9ec3285f9c861ad7a31f9b3bee6ba2a3
yur <adanyur@gmail.com>
2021-10-18 16:21:13 -0500

    se modifico la ruta de las imagenes

--- src/app/shared/components/modulos/modulos.component.ts
+++ src/app/shared/components/modulos/modulos.component.ts
@@ -10,28 +10,28 @@ export class ModulosComponent implements OnInit {
   data = [
     {
       name: 'Agenda Medica',
-      icon: '../../../../assets/icon/calendar.svg',
+      icon: 'assets/icon/calendar.svg',
     },
     {
       name: 'Programacion Sala',
-      icon: '../../../../assets/icon/medical-results.svg',
+      icon: 'assets/icon/medical-results.svg',
     },
     {
       name: 'Tarifario Medico',
-      icon: '../../../../assets/icon/calendar.svg',
+      icon: 'assets/icon/calendar.svg',
     },
     {
       name: 'Resultado Medico',
-      icon: '../../../../assets/icon/medical-results.svg',
+      icon: 'assets/icon/medical-results.svg',
     },
 
     {
       name: 'Orden Medica',
-      icon: '../../../../assets/icon/calendar.svg',
+      icon: 'assets/icon/calendar.svg',
     },
     {
       name: 'Receta Medica',
-      icon: '../../../../assets/icon/medical-results.svg',
+      icon: 'assets/icon/medical-results.svg',
     },
   ];
 
