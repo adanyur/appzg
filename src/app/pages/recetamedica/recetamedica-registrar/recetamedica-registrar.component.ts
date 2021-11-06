5c7a2799427ba813b8f1dd7a2b131dc49ece99ba
yur <adanyur@gmail.com>
2021-11-06 11:55:29 -0500

    se agrego calculos de subtotal en receta medica

--- src/app/pages/recetamedica/recetamedica-registrar/recetamedica-registrar.component.ts
+++ src/app/pages/recetamedica/recetamedica-registrar/recetamedica-registrar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { medicamentos } from '../../../core/mocks/db';
 
 @Component({
@@ -8,13 +9,18 @@ import { medicamentos } from '../../../core/mocks/db';
   templateUrl: './recetamedica-registrar.component.html',
   styleUrls: ['./recetamedica-registrar.component.css'],
 })
-export class RecetamedicaRegistrarComponent implements OnInit {
+export class RecetamedicaRegistrarComponent implements OnInit, OnDestroy {
   form: FormGroup;
   medicamentos$: Observable<any>;
   parametersSearch: string;
-
+  total$: Observable<any>;
+  private readonly unsubscribe$: Subject<void> = new Subject();
   get detalles(): FormArray {
     return this.form.get('detalles') as FormArray;
+  }
+
+  get tieneDetalle() {
+    return this.detalles.length > 0;
   }
 
   constructor(private fb: FormBuilder) {}
@@ -32,13 +38,43 @@ export class RecetamedicaRegistrarComponent implements OnInit {
   }
 
   selectMedicamento({ descripcion, precio }) {
-    this.detalles.push(
-      this.fb.group({ descripcion, precio, cantidad: 0, subtotal: 0 })
-    );
+    let item = this.fb.group({
+      descripcion,
+      precio,
+      cantidad: 0,
+      subtotal: 0,
+    });
+    this.detalles.push(item);
     this.parametersSearch = undefined;
+
+    this.detalles.valueChanges
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((_) => this.cacularSubtotal(item));
+  }
+
+  cacularSubtotal(item: FormGroup) {
+    let precio = item.get('precio').value;
+    let cantidad = item.get('cantidad').value;
+    item.get('subtotal').setValue(precio * cantidad, { emitEvent: false });
+    this.calcularTotal();
+  }
+
+  calcularTotal() {
+    this.total$ = of(
+      this.detalles.value.reduce((total, value) => total + value.subtotal, 0)
+    );
   }
 
   deleteMedicamento(indiceMedicamenti: number) {
     this.detalles.removeAt(indiceMedicamenti);
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
