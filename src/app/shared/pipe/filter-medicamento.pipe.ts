e65a0114808916523c64ca2622ce6524f98ef04f
yur <adanyur@gmail.com>
2021-11-24 13:08:01 -0500

    mejor de search receta medica

--- src/app/shared/pipe/filter-medicamento.pipe.ts
+++ src/app/shared/pipe/filter-medicamento.pipe.ts
@@ -5,9 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterMedicamentoPipe implements PipeTransform {
   transform(data: any[], searchText: string): any[] {
+    if (!data) return data;
     if (!searchText) return data;
-    return data.filter(({ numeroDeDocumento }) =>
-      numeroDeDocumento.includes(searchText.toUpperCase())
+    return data.filter(
+      ({ numeroDeDocumento, nombre, apellido }) =>
+        numeroDeDocumento.toUpperCase().includes(searchText.toUpperCase()) ||
+        nombre.toUpperCase().includes(searchText.toUpperCase()) ||
+        apellido.toUpperCase().includes(searchText.toUpperCase())
     );
   }
 }
